e9b23430b7934264929e1a353656f7fdfd8c5fc0
parikshit <singhdon85@gmail.com>
2020-02-23 01:07:49 +0530

    database connected and form values taken in

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 const path = require('path');
-
+app.use(express.urlencoded());
 
 //Importing the mail.js
 //Sending the Mail also. You can consider applying a loop to send multiple mails
 const mail = require('./mail');
 // mail.mailsend();
-mail.mailrecieve();
+// mail.mailrecieve();
 
-
+//Routes
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
 });
@@ -19,21 +19,47 @@ app.get('/comment',function(req,res){
     res.sendFile(path.join(__dirname+'/comment.html'));
 });
 
-app.get('/test',function(req,res){
-    res.sendFile(path.join(__dirname+'/table.html'));
+app.post('/change', function(req,res){
+    console.log(req.body);
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+//Static Files Used
 app.use(express.static('public'));
 
-mongoose
-    .connect('mongodb+srv://ghost:ghost@mycluster-jfog5.mongodb.net/test?retryWrites=true&w=majority')
-    .then(result => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
-    });
 
+//Database
+// mongoose
+//     .connect('mongodb+srv://ghost:ghost@mycluster-jfog5.mongodb.net/test?retryWrites=true&w=majority')
+//     .then(result => {
+//         app.listen(3000);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+
+// //Storting in database
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//     var RecordSchema = new mongoose.Schema({
+//         EmailId: String,
+//         Ticket: String,
+//         status: String
+//       });
+    
+//     var Record = mongoose.model('Record', RecordSchema);
+//     var first = new Record({ EmailId: 'akjsdkjasdh@gmail.com', Ticket: 'sahjdgajhsdjabsd', status:'open' });
+//     console.log(first);
+
+//     first.save(function (err, Record) {
+//         if (err) return console.error(err);
+//         console.log(Record + " saved to bookstore collection.");
+//     });
+// });
+
+
+//Connecting to server
 var server = app.listen(8081, function() {
     var host = server.address().address
     var port = server.address().port
